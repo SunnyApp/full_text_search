06c3293f873d54074e78bb4c41e5e3cb6a95f6f0
ericmartineau <smartytime@gmail.com>
2020-01-11 14:17:27 -0700

    Initial checkin

---
+++ lib/full_text_search.dart
@@ -0,0 +1,7 @@
+library full_text_search;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ test/full_text_search_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:full_text_search/full_text_search.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}
